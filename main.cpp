6e446245039bbbb187b2c25c3d8cb1920f24631c
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2016-03-16 15:01:31 -0500

    Added files via upload

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <ctime>
+//#include <time.h>
 #include "sort.h"
 
 void printVector(std::vector<int> V) {
@@ -13,16 +14,59 @@ void printVector(std::vector<int> V) {
     }
 }
 
-int main() {
-    std::vector<int> V;
+void timeAnalysis() {
 
+    std::vector<int> V;
     srand(time(0));
-    for (int i = 0; i < 50; i++) {
+    std::vector<int> sortedVector;
+
+    std::cout << "Size, ";
+    std::cout << "Insertion, ";
+    std::cout << "Quick\n";
+
+    for (int j = 2; j < 1000000; j = j * 2) {
+        for (int i = 0; i < j; i++) {
+            V.push_back(rand() % 100);
+        }
+
+        clock_t startTime = clock();
+        sortedVector = sort(V, "insertion");
+        //std::cout << "Size: " << V.size() << std::endl;
+        std::cout << V.size() << ", ";
+        //std::cout << "Insertion: " <<
+        std::cout << 
+            1000*(double)(clock() - startTime)/CLOCKS_PER_SEC 
+            << "ms, ";
+        sortedVector.clear();
+        V.clear();
+
+        startTime = clock();
+        sortedVector = sort(V, "quick");
+        std::cout << "Quick: " << 
+            1000*(double)(clock() - startTime)/CLOCKS_PER_SEC
+            << "ms" << std::endl;
+        sortedVector.clear();
+        V.clear();
+    }
+}
+
+int main() {
+    //std::vector<int> V;
+    timeAnalysis();
+    srand(time(0));
+    /*
+    for (int i = 0; i < 200; i++) {
         V.push_back(rand() % 100);
     }
- 
-    std::vector<int> sortedV = sort(V);
+    */
+
+    /*
+    std::vector<int> sortedV;
+    //sortedV = sort(V, "quick");
+    //printVector(sortedV);
+    sortedV = sort(V, "insertion");
     printVector(sortedV);
+    */
     return 0;
 }
 
